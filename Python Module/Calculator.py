4bde51becdea588090d10ba252228e7c60cb04ee
Rakesh23-P <patelrakesh7276@gmail.com>
2026-09-15 10:36:57 +0530

    Module

---
+++ Python Module/Calculator.py
@@ -0,0 +1,14 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subt(n1, n2):
+    return n1 - n2
+
+def mult(n1, n2):
+    return n1 * n2
+
+def dvd(n1, n2):
+    return n1 / n2  
+
+def mdvd(n1, n2):
+    return n1 % n2              
---
+++ Python Module/mymain.py
@@ -0,0 +1,12 @@
+import Calculator as cl
+import math
+
+print(cl.add(10,5))
+print(cl.subt(10,5))
+print(cl.mult(10,5))
+print(cl.dvd(10,5))
+print(cl.mdvd(10,5))
+
+
+
+print(math.sqrt(25))
